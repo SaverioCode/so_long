a5cb2fcf36f502d7dd2a3f316732617aec4f3fc1
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-03-21 18:09:24 +0100

    update

--- libsl/ft_malloc.c
+++ libsl/ft_malloc.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   ft_malloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/20 15:02:54 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/21 16:46:45 by fgarzi-c         ###   ########.fr       */
+/*   Created: 2023/03/21 16:44:59 by fgarzi-c          #+#    #+#             */
+/*   Updated: 2023/03/21 16:46:18 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
 
-# include <unistd.h>
-# include <mlx.h>
-# include <fctnl.h>
-# include "get_next_line/get_next_line.h"
-
-typedef struct s_map
+void	*ft_malloc(int bytes)
 {
-	char	**map;
-	size_t	height;
-	size_t	width;
-}	t_map;
+	void	*var;
 
-void	ft_error(char *str);
-size_t	ft_strlen(char *str);
-void	ft_check_input(int ac, char **av);
-void	*ft_malloc(int bytes);
-
-#endif
+	var = malloc(bytes);
+	if (!var)
+		exit(0);
+	return (var);
+}
--- libsl/map_handler.c
+++ libsl/map_handler.c
@@ -6,10 +6,52 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:36:31 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/21 15:44:33 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/21 16:56:17 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	ft_map_checker()
+{
+	
+}
 
+void	ft_map_allocate(int	fd)
+{
+	t_map	*map;
+	int		i;
+
+	map->map = ft_malloc((map->height + 1) * 8);
+	i = 0;
+	while (i < map->heigth)
+	{
+		map->map[i] = get_next_line(fd);
+		map->map[i][ft_strlen(map->map[i])] = 0;
+		i++; 
+	}
+	map->map[i] = NULL;
+}
+
+void	ft_map_reader()
+{
+	t_map	*map; /// check if it's ok
+	int		fd;
+	char	*line;
+
+	map->height = 0;
+	fd = open(map, O_RDONLY);
+	if (fd < 0)
+		ft_error("Error: map not found!\n");
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		map->height++;
+		free(line);
+	}
+	ft_map_allocate(fd);
+	map->width = ft_strlen(map->map[0]);
+	close(fd);
+}
