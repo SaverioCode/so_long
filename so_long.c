ff600d475444472d526d8dea2d1e9acb33569222
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-03-26 20:55:06 +0200

    update

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:02:42 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/26 20:45:24 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/26 20:54:59 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int ac, char **av)
 	ft_map_handler(av[1], &game);
 	game.mv_cnt = 0;
 	game.game = mlx_init();
-	game.win = mlx_new_window(game.game, game.width * 50, game.height * 50, "SO_LONG"); /// to modify
+	game.win = mlx_new_window(game.game, game.width * 50, game.height * 50, "SO_LONG");
 	map_printer(&game, &img);
 	mlx_key_hook(game.win, (void *)ft_key_hook, &game);
 	mlx_loop(game.game);
